cb58118bf234e7b07e7695a235d091b95c19b346
DavidBogu <45219435+DavidBogu@users.noreply.github.com>
2022-09-17 15:39:26 -0700

    MainMenuFunctionalityDone

---
+++ PAFIS/Assets/MainMenu.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class MainMenu : MonoBehaviour
+{
+    public string gameScene;
+    public string settingsScene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void startGame()
+    {
+        SceneManager.LoadScene(gameScene);
+    }
+
+    public void settings()
+    {
+        SceneManager.LoadScene(settingsScene);
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+}
