486a7e3744693bce124b2db91b44903071d7b816
RyanmLin <64663423+RyanmLin@users.noreply.github.com>
2022-09-18 14:06:48 -0700

    Added Spawner

---
+++ Assets/RandomSpawn.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpawn : MonoBehaviour
+{
+    public float delay;
+    public GameObject spawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnDelay(delay));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator SpawnDelay(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        Instantiate(spawn);
+    }
+}
